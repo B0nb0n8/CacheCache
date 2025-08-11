263a549f8194a8feaba7b4a87b427b33f4470f91
B0nb0n8 <besscami0811@gmail.com>
2025-08-11 20:12:49 +0200

    Script car
    я написала скрипт для движения машины, и добавила точки следования

--- Assets/Scipts/CarController.cs
+++ Assets/Scipts/CarController.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class CarController : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float speed = 10f;
-    public float turnSpeed = 50f;
+    [SerializeField] Transform[] wayPoints;
+    [SerializeField] private float speed = 3f;
+    private float chekpointDistance = 0.5f;
+    private int currentChekpoint = 0;
 
     private Rigidbody rb;
 
@@ -18,11 +20,19 @@ public class CarController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        float moveInput = Input.GetAxis("Vertical");
-        float turnInput = Input.GetAxis("Horizontal");
+       Vector3 Target = wayPoints[currentChekpoint].position;
+       Vector3 newPos = Vector3.MoveTowards(gameObject.transform.position, Target, speed * Time.fixedDeltaTime);
 
-        Vector3 move = transform.forward * moveInput * speed * Time.fixedDeltaTime;
-        rb.MovePosition(rb.position + move);
-        rb.MoveRotation(rb.rotation * turn);
+       rb.MovePosition(newPos);
+
+        if (Vector3.Distance(gameObject.transform.position, wayPoints[currentChekpoint].position) <=  chekpointDistance)
+        {
+            currentChekpoint++; //currentChekpoint = currentChekpoint + 1
+            if (currentChekpoint >= wayPoints.Length - 1)
+            {
+                currentChekpoint = 0;
+            }
+        }
+
     }
 }
