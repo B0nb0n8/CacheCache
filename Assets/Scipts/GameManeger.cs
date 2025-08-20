58f5a6a10227ac447bf708f3a378abce5093cf93
B0nb0n8 <besscami0811@gmail.com>
2025-08-20 16:57:03 +0200

    Карта
    Я доделала карту.

--- Assets/Scipts/GameManeger.cs
+++ Assets/Scipts/GameManeger.cs
@@ -1,38 +1,97 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class GameManeger : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    [SerializeField] GameObject[] hiddenDogs;
-    [SerializeField] Transform[] spawnPoints;
-      
+    Rigidbody rb;
+    [SerializeField] Animator anim;
+    [SerializeField] float shiftSpeed = 10f;
 
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float movementSpeed = 5f;
+    Vector3 direction;
+    float currentSpeed;
+    float stamina = 5f;
+
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private Image crosshairImage;
+    [SerializeField] private float rayDistance = 100f;
+    private Color defaultColor = Color.white;
+    private Color highlightColor = Color.green;
+    void Start ()
     {
-        HideDogs();
+        rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
+        currentSpeed = movementSpeed;
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        direction = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        direction = transform.TransformDirection(direction);
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            if (stamina > 0)
+            {
+                stamina -= Time.deltaTime;
+                currentSpeed = shiftSpeed;
+            }
+            else
+            {
+                currentSpeed = movementSpeed;
+            }
+        }
+        else if (!Input.GetKey(KeyCode.LeftShift))
+        {
+            stamina += Time.deltaTime;
+            currentSpeed = movementSpeed;
+        }
+        if (stamina > 5f)
+        {
+            stamina = 5f;
+        }
+        else if (stamina < 0)
+        {
+            stamina = 0;
+        }
+
+        ChangeAnimations();
+    }
+    void FixedUpdate()
+    {
+        rb.MovePosition(transform.position + direction * currentSpeed * Time.fixedDeltaTime);
+        DetectDog();
     }
 
-    private void HideDogs()
+    private void ChangeAnimations()
     {
-        List<Transform> availablePoints = new List<Transform>(spawnPoints);
+        anim.SetBool("Walk", (direction.x != 0 || direction.z != 0) && currentSpeed == movementSpeed);
+        anim.SetBool("Run", (direction.x != 0 || direction.z != 0) && currentSpeed == shiftSpeed);
+        anim.SetBool("Wiggling", direction.x == 0 && direction.z == 0);
+    }
 
-        foreach (GameObject dog in hiddenDogs)
+    private void DetectDog()
+    {
+        Vector3 screenCenter = new Vector3(Screen.width / 2f, Screen.height / 2f, 0f);
+        Ray ray = mainCamera.ScreenPointToRay(screenCenter);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, rayDistance))
         {
-            int index = Random.Range(0, availablePoints.Count);
-            Transform point = availablePoints[index];
-
-            dog.transform.position = point.position;
-            dog.transform.rotation = point.rotation;
-
-            availablePoints.RemoveAt(index);
+            if (hit.collider.CompareTag("HiddenDog"))
+            {
+                crosshairImage.color = highlightColor;
+            }
+            else
+            {
+                crosshairImage.color = defaultColor;
+            }
+        }
+        else
+        {
+            crosshairImage.color = defaultColor;
         }
     }
-}
